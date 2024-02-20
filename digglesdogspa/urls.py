f935b33e9d9e58a9ef68f354e703f93985170b43
Chris Diggle <chrisdiggle8@outlook.com>
2024-02-20 17:19:49 +0000

    Create project and app, test a http response

---
+++ digglesdogspa/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for digglesdogspa project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from booking_system.views import my_booking_system
+
+urlpatterns = [
+    path('booking_system/', my_booking_system, name='booking system'),
+    path('admin/', admin.site.urls),
+]
