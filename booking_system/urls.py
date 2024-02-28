223204e7654acd0f0fdc9811ce7f126c049da546
Chris Diggle <chrisdiggle8@outlook.com>
2024-02-28 19:53:00 +0000

    amend home page from displaying my account content

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import BookingsList
 
 urlpatterns = [
-    path("", views.BookingsList.as_view(), name='home'),
+    path("", views.home, name='home'),
     path('about/', views.about, name='about'),
     path('price-list/', views.price_list, name='price_list'),
     path('register/', views.register, name='register'),
