28dfddfb227f08d12c1defeab9767db5594fa51d
Chris Diggle <chrisdiggle8@outlook.com>
2024-02-28 15:35:06 +0000

    add urls file, and ndex.html, create header and footer

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
 
 # Create your views here.
+class HomeView(generic.ListView):
+    queryset = Booking.objects.all()
+    template_name = "index.html"
