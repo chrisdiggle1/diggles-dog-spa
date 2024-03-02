16c65f0dde5ba55b581b497e67ed86de204555bc
Chris Diggle <chrisdiggle8@outlook.com>
2024-03-02 12:26:10 +0000

    install cloudinary and add main image to home page

--- digglesdogspa/settings.py
+++ digglesdogspa/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-chrisdiggle-digglesdogs-9lmwi9ubth7.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django_summernote',
+    'cloudinary',
     'booking_system',
 ]
 
