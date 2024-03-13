dd7b3c47d52de23c1f26ac36b54b8e3edba669cc
Chris Diggle <chrisdiggle8@outlook.com>
2024-03-13 08:16:48 +0000

    link up book button on service card to booking page

--- digglesdogspa/settings.py
+++ digglesdogspa/settings.py
@@ -27,10 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-chrisdiggle-digglesdogs-9lmwi9ubth7.ws-eu108.gitpod.io', '.herokuapp.com']
+    '8000-chrisdiggle-digglesdogs-9lmwi9ubth7.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
