f3f45aa82515d80a8eafc91b9d0391d65a50410c
Chris Diggle <chrisdiggle8@outlook.com>
2024-03-16 10:34:52 +0000

    wire up add service form and make responsive on all devices

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -16,3 +16,9 @@ class ServiceForm(forms.ModelForm):
     class Meta:
         model = Services
         fields = ['service_name', 'cost', 'description', 'duration']
+        widgets = {
+            'service_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'cost': forms.Textarea(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'duration': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'HH:MM:SS'}),
+        }
